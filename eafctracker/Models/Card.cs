6a242b915c7f8045b7ee8fcaa4883d90abc3ee1b
Vasjen Mukhin <vasjenm@gmail.com>
2023-10-24 19:48:14 +0300

    Add models

---
+++ eafctracker/Models/Card.cs
@@ -0,0 +1,22 @@
+
+
+namespace Eafctracker.Models
+{
+    public class Card
+    {
+        
+        public int Id { get; init; }
+        public int FbId { get; init; }
+        public int FbDataId { get; init; }
+        public string DisplayedName { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public int PcId { get; set; }
+        public Pc PcPrices { get; set; }
+        public int PsId { get; set; }
+        public Ps PsPrices {get;set;}
+        public string Position { get; set; } = string.Empty;
+        public string Revision { get; set; } = string.Empty;
+        public int Raiting { get; set; }
+
+    }
+}
